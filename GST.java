3dd4cd85e4c381c3068b5b79dc9b3181a493cd7a
udayrg1 <107872747+udayrg1@users.noreply.github.com>
2022-07-15 20:11:39 +0530

    Add files via upload

---
+++ GST.java
@@ -0,0 +1,27 @@
+package GST;
+
+
+/* java program to compute GST 
+ * from original price & net price */
+public class GST {
+	
+	static float Calculate_GST(float org_cost, float N_price)
+	
+	{ 
+		// return value after calculating GST%
+		return (((N_price - org_cost) * 100) / org_cost);
+		
+	}
+
+	
+	public static void main (String[] args)
+	
+	{
+		float org_cost = 15000;
+				float N_price = 18000;
+				System.out.println("GST = " + Calculate_GST
+						(org_cost, N_price) + " % ");
+	
+	}
+
+	}
